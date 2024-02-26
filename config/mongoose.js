fc9992c794a1fa469ad680507b5a1ba2624c7099
veenit1802 <veenitshukla20@gmail.com>
2024-02-26 22:26:55 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const DB = 'mongodb://localhost:27017';
+
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = mongoose;
